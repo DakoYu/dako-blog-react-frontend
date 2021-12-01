f68e42f3cc0fb62d20472e95f252855409d003dd
DakoYu <89088363+DakoYu@users.noreply.github.com>
2021-11-30 23:16:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Cookies from 'universal-cookie';
 import Navbar from './components/Navbar';
 import Login from './components/Login/Login';
 import Register from "./components/Register/Register";
@@ -12,17 +11,6 @@ import './App.css';
 
 function App() {
     const AuthCtx = useContext(AuthContext);
-  
-    const cookies = new Cookies();
-  
-
-   useEffect(() => {
-     if (cookies.get('jwt')) {
-       alert('test');
-       AuthCtx.login();
-     }
-   }, [AuthCtx.userState.isLogged])
-  
 
   return (
     <React.Fragment>
